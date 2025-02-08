f497b877900188de5ef2a9967baa3aef13a8c06e
Abhishek Patil <patilabhishek394@gmail.com>
2025-02-08 20:00:52 +0530

    Add social medis URLs in the project.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,27 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 size-15 width-10" src={logo} alt="Profile-Logo" />
+        <img className="mx-2 w-10 h-10" src={logo} alt="Profile-Logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
+        <FaLinkedin
+          className="cursor-pointer"
+          onClick={() =>
+            window.open("https://www.linkedin.com/in/nilshiv/", "_blank")
+          }
+        />
+        <FaGithub
+          className="cursor-pointer"
+          onClick={() =>
+            window.open("https://www.github.com/NilShiv/", "_blank")
+          }
+        />
+        <FaInstagram
+          className="cursor-pointer"
+          onClick={() =>
+            window.open("https://www.instagram.com/nil_shiv_/", "_blank")
+          }
+        />
       </div>
     </nav>
   );
