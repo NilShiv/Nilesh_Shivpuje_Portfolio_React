1dcff974588c209aba3ed97529302f478d26d984
Abhishek Patil <patilabhishek394@gmail.com>
2025-02-09 09:53:48 +0530

    Add about section in the portfolio.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import profilePic from "../assets/nileshShivpujeProfile.png";
 
 const Hero = () => {
   return (
-    <div className="pb-4 lg:mb-35">
+    <div className="border-b border-neutral-900 pb-8 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
