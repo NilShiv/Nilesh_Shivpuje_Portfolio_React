8c818b2bda4e93ac2dc72702785e8b9ce8823e08
NilShiv <nilshiv000@gmail.com>
2025-03-06 20:41:06 +0530

    made changes in vite config file, remove unwanted line of code

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/nil-port", // Adjust this if needed for proper routing
-  build: {
-    outDir: "dist", // Ensure the output folder is explicitly set to "dist"
-    emptyOutDir: true, // Clean the output directory before building
-    sourcemap: true, // Optional: Helps with debugging
-  },
+  plugins: [react(), tailwindcss()]
 })
